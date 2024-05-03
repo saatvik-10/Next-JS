9ca7f03c38aff965b3b834f9bb0b3a805644a07f
saatvik-10 <saatvikmadan409@gmail.com>
2024-05-04 01:16:54 +0530

    added sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,97 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import { useToast } from '@/components/ui/use-toast';
+import { useRouter } from 'next/navigation';
+import { signInSchema } from '@/schemas/signInSchema';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { signIn } from 'next-auth/react';
 
 const page = () => {
-  return <div>SIGN IN PAGE</div>;
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentails', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    if (result?.error) {
+      toast({
+        title: 'Login Failed',
+        description: 'Incorrect username or password',
+        variant: 'destructive',
+      });
+    }
+
+    if (result?.url) {
+      router.replace('/dashboard');
+    }
+  };
+
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-xl shadow-md'>
+        <div className='text-center'>
+          <h1 className='text-4-xl font-extrabold tracking-light lg:text-5xl mb-6'>
+            Join Mystrey Message
+          </h1>
+          <p className='mb-4'>Sign In to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <FormField
+              name='identifier'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder='username / email' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='identifier'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type='password' placeholder='password' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Sign in</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
-import { signUpSchema } from './signUpSchema';
 
 export const signInSchema = z.object({
-  identifiers: z.string(),
+  identifier: z.string(),
   password: z.string(),
 });
