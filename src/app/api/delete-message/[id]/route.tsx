ae05a486874ecc9463f73486a96be888e11d4eb4
saatvik-10 <saatvikmadan409@gmail.com>
2024-05-08 03:14:20 +0530

    added delete route

---
+++ src/app/api/delete-message/[id]/route.tsx
@@ -0,0 +1,56 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../auth/[...nextauth]/options';
+import dbConnect from '@/lib/dbConnect';
+import UserModel from '@/model/User';
+import { User } from 'next-auth';
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { messageid: string } }
+) {
+  const messageId = params.messageid;
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || session?.user) {
+    return Response.json(
+      {
+        success: false,
+        message: 'Not Authenticated',
+      },
+      { status: 401 }
+    );
+  }
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+    if (updateResult.modifiedCount == 0) {
+      return Response.json(
+        {
+          success: false,
+          message: 'Message not found',
+        },
+        { status: 404 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: 'Message Deleted',
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    return Response.json(
+      {
+        success: false,
+        message: 'Error deleted the message',
+      },
+      { status: 500 }
+    );
+  }
+}
